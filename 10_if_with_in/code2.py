3b67cb11ad1a2e7605ebb2dd85357a0ee8da6ce9
gustavofariaas <gustavofariasvn@hotmail.com>
2022-01-10 15:32:41 -0300

    adding a new code2

---
+++ 10_if_with_in/code2.py
@@ -0,0 +1,11 @@
+number = 7
+user_play = input("Press 'y' if you want to play: ").lower()
+
+if user_play == "y":
+    user_number = int(input("Please enter a number: "))
+    if user_number == number:
+        print("You have enter the correct number")
+    elif user_number - number in (1,-1):
+        print("You miss by one number")
+    else:
+        print("You got the wrong number")
