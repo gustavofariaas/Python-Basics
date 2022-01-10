969f7bc6d9c7baa3666c752f3decc5e4e8356b33
gustavofariaas <gustavofariasvn@hotmail.com>
2022-01-10 15:09:02 -0300

    Adding changes

--- 10_if_with_in/code.py
+++ 10_if_with_in/code.py
@@ -1,1 +1,7 @@
-print("test")
+movies_watched = ["The Matrix", "Dumbo"]
+user_input = input("Please enter the movie you've watched: ")
+
+if user_input in movies_watched:
+    print(f"You have already watched {user_input}")
+else:
+    print("This has been the first time you've watched this movie")
